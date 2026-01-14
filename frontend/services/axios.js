2d70d657af4385a12f52c64693fb2166b7320a6d
Mubashir Hussain <mubashirmh04@gmail.com>
2026-01-14 13:12:26 +1100

    fixing baseUrl for deployment

--- frontend/services/axios.js
+++ frontend/services/axios.js
@@ -5,8 +5,7 @@ const LOCAL_URL = "http://localhost:3229";
 const PROD_URL = Constants.expoConfig?.extra?.apiUrl || "https://logisticsapp-uldj.onrender.com";
 
 // Automatically use local in dev, production in builds
-const BASE_URL = LOCAL_URL;
-
+const BASE_URL = __DEV__ ? LOCAL_URL : PROD_URL;
 console.log('Using API URL:', BASE_URL, __DEV__ ? '(LOCAL)' : '(PRODUCTION)');
 
 
