b7cd10dbcab084bd61a02250b270e453ac0acb1c
Mubashir Hussain <mubashirmh04@gmail.com>
2026-01-16 14:50:46 +1100

    using asyncStorage for persistently storing accessTokens

--- frontend/components/AuthProvider.jsx
+++ frontend/components/AuthProvider.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AuthContext } from './AuthContext'
 
 function AuthProvider({children}) {
@@ -6,14 +7,66 @@ function AuthProvider({children}) {
     const [userId, setUserId] = useState("")
     const [accessToken, setAccessToken] = useState("")
     const [role, setRole] = useState("")
+    const[isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const loadAuth = async () => {
+            try {
+                const savedAccessToken = await AsyncStorage.getItem('accessToken');
+                const savedUserRole = await AsyncStorage.getItem('role');
+                const savedUserId = await AsyncStorage.getItem('userId');
+
+                if (savedUserId) setUserId(savedUserId);
+                if (savedAccessToken) setAccessToken(savedAccessToken);
+                if(savedUserRole) setRole(savedUserRole);
+            } catch (error) {
+                console.error('Failed to load Auth', error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        loadAuth();
+    }, [])
+
+    const persistSetAccessToken = async(token) => {
+        setAccessToken(token);
+        if (token) {
+            await AsyncStorage.setItem('accessToken', token);
+        } else {
+            await AsyncStorage.removeItem('accessToken');
+        }
+    }
+
+    const persistSetUserRole = async(role) => {
+        setRole(role);
+        if (role) {
+            await AsyncStorage.setItem('role', role);
+        } else {
+            await AsyncStorage.removeItem('role');
+        }
+    }
+
+    const persistSetUserId = async(id) => {
+        setUserId(id);
+        if (id) {
+            await AsyncStorage.setItem('userId', id);
+        } else {
+            await AsyncStorage.removeItem('userId');
+        }
+    }
+
+    if (isLoading) {
+        return null; // or a loading spinner
+    }
 
     const contextValue = {
         userId, 
-        setUserId,
+        persistSetUserId,
         accessToken,
-        setAccessToken,
+        persistSetAccessToken,
         role,
-        setRole
+        persistSetUserRole
     }
 
     return (
