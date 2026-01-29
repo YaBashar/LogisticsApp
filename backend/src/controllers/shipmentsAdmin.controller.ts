6e0beeb0d9f9aa1fc7b55b456f015d2d30017a21
Mubashir Hussain <mubashirmh04@gmail.com>
2026-01-29 22:43:53 +1100

    Implementing pagination for admin getAllActiveOrders enpoint

--- backend/src/controllers/shipmentsAdmin.controller.ts
+++ backend/src/controllers/shipmentsAdmin.controller.ts
@@ -3,8 +3,12 @@ import { Request, Response } from "express";
 
 
 export const getAllActiveOrders = async(req: Request, res: Response) => {
+
+    const page = parseInt(req.query.page as string);
+    const limit = parseInt(req.query.limit as string);
+
     try {
-        const result = await allActiveOrders();
+        const result = await allActiveOrders(page, limit);
         res.status(200).json({result})
     } catch (error) {
         res.status(400).json({error: error.message});
--- backend/src/service/shipmentsAdmin.ts
+++ backend/src/service/shipmentsAdmin.ts
@@ -1,9 +1,13 @@
 import { ShipmentModel } from "../models/shipmentsModel";
 
 
-async function allActiveOrders() {
-    const shipments = await ShipmentModel.find({completed: false})
-        .populate('userId', 'name');
+async function allActiveOrders(page: number, limit: number) {
+    const shipments = await ShipmentModel
+        .find({completed: false})
+        .populate('userId', 'name')
+        .skip((page - 1) * limit)
+        .limit(limit);
+
     console.log(shipments);
     return shipments;
 }
