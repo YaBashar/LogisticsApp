63f9a9f386371965065b1811b9c582b15fbb8dc3
Mubashir Hussain <mubashirmh04@gmail.com>
2026-02-03 11:55:24 +1100

    Completed push notification setup on frontend

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,16 @@
 import { Stack } from "expo-router";
 import { useEffect } from "react";
 import { AuthProvider } from "../components/AuthProvider";
+import { usePushNotifs } from "@/hooks/usePushNotifs";
 import * as SplashScreen from "expo-splash-screen";
 
 // Prevent splash screen from auto-hiding
+
+function AppContent() {
+  usePushNotifs();
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
+
 SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   useEffect(() => {
@@ -15,7 +22,7 @@ export default function RootLayout() {
 
   return (
     <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }} />
+      <AppContent />
     </AuthProvider>
   );
 }
