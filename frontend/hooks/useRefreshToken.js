6046544f1854ada479bc27f6184fa50b8c76a76d
Mubashir Hussain <mubashirmh04@gmail.com>
2026-02-08 19:16:31 +1100

    Updating refreshToken mechanism to use secureStorage

--- frontend/hooks/useRefreshToken.js
+++ frontend/hooks/useRefreshToken.js
@@ -1,12 +1,19 @@
 import axios from "../services/axios";
+import * as SecureStorage from "expo-secure-store";
 
 const useRefreshToken = () => {
   const refresh = async () => {
-    const response = await axios.post("/auth/refresh", {
-      withCredentials: true,
-    });
+    const refreshToken = await SecureStorage.getItemAsync("refreshToken");
 
-    const accessToken = response.data.token;
+    if (!refreshToken) {
+      throw new Error("No refresh Token available");
+    }
+
+    console.log("Refreshing with token:", refreshToken);
+    const response = await axios.post("/auth/refresh", { refreshToken });
+    const { accessToken, refreshToken: newRefreshToken } = response.data;
+
+    await SecureStorage.setItemAsync("refreshToken", newRefreshToken);
     return accessToken;
   };
 
