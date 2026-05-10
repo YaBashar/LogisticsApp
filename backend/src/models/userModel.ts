b2681edac416faa5c83e180697b15b4ad93f9d1a
Mubashir Hussain <mubashirmh04@gmail.com>
2026-05-10 15:54:59 +1000

    models: update fields and add type safety

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,27 +1,50 @@
-import mongoose from "mongoose";
-import { User } from "./interfaces";
+import mongoose, { Document } from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true },
-  refreshTokens: { type: [String], default: [] },
-  pushTokens: { type: [String], default: [] },
-  role: {
-    type: String,
-    enum: ["admin", "customer"],
-    default: "customer",
+export type UserRole = "customer" | "admin";
+
+export interface User extends Document {
+  id: string;
+  name: string;
+  password: string;
+  email: string;
+  role: UserRole;
+  loginAttempts: number;
+  lockUntil?: Date;
+  accountLocked: boolean;
+  verificationCode?: string | null;
+  verificationCodeExpiry?: Date | null;
+  resetCode?: string | null;
+  resetCodeExpiry?: Date | null;
+  emailVerified: boolean;
+  accountExpiresAt?: Date | null;
+  deletedAt?: Date | null;
+  createdAt: Date;
+  updatedAt: Date;
+  numSuccessfulLogins: number;
+  numfailedSinceLastLogin: number;
+  passwordHistory: string[];
+  pushTokens: string[];
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    role: { type: String, enum: ["customer", "admin"], default: "customer" },
+    loginAttempts: { type: Number, default: 0 },
+    lockUntil: { type: Date },
+    accountLocked: { type: Boolean, default: false },
+    verificationCode: { type: String },
+    verificationCodeExpiry: { type: Date },
+    resetCode: { type: String },
+    resetCodeExpiry: { type: Date },
+    emailVerified: { type: Boolean, default: false },
+    accountExpiresAt: { type: Date, default: null },
+    deletedAt: { type: Date, default: null },
+    pushTokens: { type: [String], default: [] },
   },
-  loginAttempts: { type: Number, default: 0 },
-  lockUntil: { type: Date },
-  accountLocked: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-  verificationCode: { type: String, unique: true, sparse: true },
-  verificationCodeExpiry: { type: Date },
-  resetCode: { type: String },
-  resetCodeExpiry: { type: Date },
-  emailVerified: { type: Boolean, default: false },
-});
+  { timestamps: true }
+);
 
 export const UserModel = mongoose.model<User>("User", userSchema);
